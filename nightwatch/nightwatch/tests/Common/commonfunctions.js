097cdd99fb0586f5bfeb5b311f9557cf7bcd6ba7
pavanprakash <pavanprakash@Pavans-MBP.home>
2017-03-05 21:33:38 +0000

    changed night watch.json file

--- nightwatch/nightwatch/tests/Common/commonfunctions.js
+++
@@ -1,8 +0,0 @@
-///**
-// * Created by Pavan.Prakash on 04/08/2014.
-// */
-//
-////** commonly used variables**************
-//
-//var preliveurl="http://prelive-hotels4uv2.h4u.me/"
-//var testurl="http://test-hotels4uv2.h4u.me/"
